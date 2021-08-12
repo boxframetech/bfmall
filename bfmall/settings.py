149ba5d6a59b2df7a74451c67885fdf292b06b0e
boxframetech <87312521+boxframetech@users.noreply.github.com>
2021-08-12 15:21:29 +0000

    Update settings.py

--- bfmall/settings.py
+++ bfmall/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'w)8$ujum2+17ihzi+k$69t0vb7aj-w+ypqjyo=^#m4(08bd=rt'
+SECRET_KEY = 'keep the secret key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
